ed6d05f021dc9757f7b9db53aad6e7c50a57f866
JuliaGord <jg0311@bk.ru>
2017-04-19 21:59:55 +0300

    Добавила вложенные функции в главную

--- js/stat.js
+++ js/stat.js
@@ -1,34 +1,43 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(110, 20, 420, 270);
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = '#fff';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
   var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
-  var histogramWidth = 150;
-  var step = histogramWidth / max;
-  var indent = 50;
-  var barWidth = 40;
-  var initialX = 120;
-  var cloudHeight = 270;
-  ctx.textBaseline = 'top';
-  for (var j = 0; j < times.length; j++) {
-    ctx.fillRect(initialX + (indent + barWidth) * j, cloudHeight - times[j] * step - 30, barWidth, times[j] * step);
+  var drawCloud = function () {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.strokeRect(110, 20, 420, 270);
+    ctx.fillRect(110, 20, 420, 270);
+    ctx.fillStyle = '#fff';
+    ctx.strokeRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270); 
+  };
+  var writeText = function () {
     ctx.fillStyle = '#000';
-    ctx.fillText(times[j].toFixed(0), initialX + (indent + barWidth) * j, cloudHeight - times[j] * step - 50);
-    ctx.fillText(names[j], initialX + (indent + barWidth) * j, cloudHeight - 20);
-  }
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
+  };
+  var histo = function () {
+    var indent = 50;
+    var barWidth = 40;
+    var initialX = 120;
+    var cloudHeight = 270;
+    ctx.textBaseline = 'top';
+    for (var i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+      }
+    }
+    var histogramWidth = 150;
+    var step = histogramWidth / max;
+    for (var i = 0; i < times.length; i++) {
+      ctx.fillRect(initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 30, barWidth, times[i] * step);
+      ctx.fillStyle = '#000';
+      ctx.fillText(times[i].toFixed(0), initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 50);
+      ctx.fillText(names[i], initialX + (indent + barWidth) * i, cloudHeight - 20);
+    }
+  };
+  drawCloud();
+  writeText();
+  histo();
 };
