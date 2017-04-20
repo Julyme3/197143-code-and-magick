f523f84d07b3010070df911c9be488303bdb13f0
JuliaGord <jg0311@bk.ru>
2017-04-20 21:34:05 +0300

    Изменены функции, оформлена гистограмма

--- js/stat.js
+++ js/stat.js
@@ -2,42 +2,49 @@
 
 window.renderStatistics = function (ctx, names, times) {
   var max = -1;
-  var drawCloud = function () {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.strokeRect(110, 20, 420, 270);
-    ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = '#fff';
-    ctx.strokeRect(100, 10, 420, 270);
-    ctx.fillRect(100, 10, 420, 270); 
+  var indent = 50;
+  var barWidth = 40;
+  var initialX = 120;
+  var cloudHeight = 270;
+  var currentColor = '#000000';
+  var histogramWidth = 150;
+
+  var drawRect = function (x1, y1, x2, y2, color) {
+    ctx.fillStyle = color || '#000000';
+    ctx.fillRect(x1, y1, x2, y2);
   };
-  var writeText = function () {
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', 120, 40);
-    ctx.fillText('Список результатов:', 120, 60);
+
+  var drawText = function (text, x, y, color, font) {
+    ctx.fillStyle = color || '#000000';
+    ctx.font = font || '16px PT Mono';
+    ctx.fillText(text, x, y);
   };
-  var histo = function () {
-    var indent = 50;
-    var barWidth = 40;
-    var initialX = 120;
-    var cloudHeight = 270;
-    ctx.textBaseline = 'top';
-    for (var i = 0; i < times.length; i++) {
-      var time = times[i];
-      if (time > max) {
-        max = time;
-      }
+
+  drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  drawRect(100, 10, 420, 270, '#ffffff');
+
+  drawText('Ура вы победили!', 120, 40);
+  drawText('Список результатов:', 120, 60);
+
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
     }
-    var histogramWidth = 150;
-    var step = histogramWidth / max;
-    for (var i = 0; i < times.length; i++) {
-      ctx.fillRect(initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 30, barWidth, times[i] * step);
-      ctx.fillStyle = '#000';
-      ctx.fillText(times[i].toFixed(0), initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 50);
-      ctx.fillText(names[i], initialX + (indent + barWidth) * i, cloudHeight - 20);
+  }
+
+  var step = histogramWidth / max;
+
+  for (i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
+      currentColor = 'rgba(255, 0, 0, 1)';
+    } else {
+      currentColor = 'rgb(0, 0, ' + Math.round(Math.random() * 255) + ')';
     }
-  };
-  drawCloud();
-  writeText();
-  histo();
+
+    drawRect(initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 30, barWidth, times[i] * step, currentColor);
+    drawText(times[i].toFixed(0), initialX + (indent + barWidth) * i, cloudHeight - times[i] * step - 35);
+    drawText(names[i], initialX + (indent + barWidth) * i, cloudHeight - 10);
+  }
+
 };
